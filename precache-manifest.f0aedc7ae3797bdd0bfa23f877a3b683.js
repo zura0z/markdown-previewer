9971985f6bce33bedbc7587c9c3cda49e760e91e
zura0z <zuramazmishvili@gmail.com>
2020-10-01 15:51:32 +0400

    Updates

---
+++ precache-manifest.f0aedc7ae3797bdd0bfa23f877a3b683.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5e0607432b33414e60f5280a72cdc97",
+    "url": "/markdown-previewer/index.html"
+  },
+  {
+    "revision": "a10c239763fa95966507",
+    "url": "/markdown-previewer/static/css/main.dbe512ff.chunk.css"
+  },
+  {
+    "revision": "7b710db392decd22e935",
+    "url": "/markdown-previewer/static/js/2.8f878d59.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/markdown-previewer/static/js/2.8f878d59.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a10c239763fa95966507",
+    "url": "/markdown-previewer/static/js/main.4a840784.chunk.js"
+  },
+  {
+    "revision": "380a9cf99d31db8da525",
+    "url": "/markdown-previewer/static/js/runtime-main.4fd0b226.js"
+  }
+]);
